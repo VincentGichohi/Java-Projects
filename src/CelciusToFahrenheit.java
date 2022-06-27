67f225daa252633cc6dfe5a0af53eb73b8280b32
VincentGichohi <vincentegichohi@gmail.com>
2022-06-27 12:49:26 +0300

    Make some changes to the CelciusToFahreneit class

--- src/CelciusToFahrenheit.java
+++ src/CelciusToFahrenheit.java
@@ -1,2 +1,20 @@
+import java.util.Scanner;
+
 public class CelciusToFahrenheit {
+
+    public static void main(String[] args) {
+        var keyboard = new Scanner(System.in);
+        double celcius, fahreneit;
+
+        System.out.print("Enter the Celcius temperature: ");
+        celcius =  keyboard.nextDouble();
+
+        fahreneit = 9.0 /5.0 * celcius + 32.0;
+
+        System.out.print("Room temperature? ");
+        System.out.println(fahreneit == 69.8);
+        System.out.print(fahreneit);
+
+        keyboard.close();
+    }
 }
