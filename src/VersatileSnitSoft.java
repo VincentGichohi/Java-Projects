8079ac2e13b0343596e8c6d6849b3d4f8707c9d9
VincentGichohi <vincentegichohi@gmail.com>
2022-06-27 11:59:20 +0300

    Add a versatilesnitsoft class

--- src/VersatileSnitSoft.java
+++ src/VersatileSnitSoft.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class VersatileSnitSoft {
+import java.util.Scanner;
+
+public class VersatileSnitSoft {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        double amount;
+
+        System.out.print("What's the price of the flash drive? ");
+        amount = keyboard.nextDouble();
+        amount = amount + 25.00;
+
+        System.out.print("We will bill $");
+        System.out.print(amount);
+        System.out.println(" to your credit card.");
+
+        keyboard.close();
+    }
 }
