9fc2e68bc65c983a8f763cdf401fd1ab87f6d340
VincentGichohi <vincentegichohi@gmail.com>
2022-06-27 11:48:49 +0300

    Add new java class

--- src/CanIKeepKidsQuiet.java
+++ src/CanIKeepKidsQuiet.java
@@ -1,2 +1,19 @@
 public class CanIKeepKidsQuiet {
+
+    public static void main(String[] args) {
+        int gumballs;
+        int kids;
+        int gumballsPerKid;
+        boolean eachKidGetsTen;
+
+        gumballs = 140;
+        kids = 15;
+        gumballsPerKid = gumballs / kids;
+
+        System.out.print("True or False? ");
+        System.out.println("Each kid gets 10 gumballs. ");
+        eachKidGetsTen = gumballsPerKid >= 10;
+        System.out.println(eachKidGetsTen);
+
+    }
 }
