1a0360cc5d100d977e5233cfdf8d01cad1d7e166
VincentGichohi <vincentegichohi@gmail.com>
2022-06-27 11:58:35 +0300

    Add a snitsoft class

--- src/SnitSoft.java
+++ src/SnitSoft.java
@@ -1,2 +1,79 @@
-package PACKAGE_NAME;public class SnitSoft {
+
+public class SnitSoft {
+    public static void main(String[] args) {
+        //create a variable called amount
+        double amount;
+        //a variable called amount is assigned a value
+        amount = 5.95;
+        amount = amount + 25.00;
+
+        System.out.println("We will bill $");
+        System.out.println(amount);
+        System.out.println(" to your credit card");
+    }
 }
+//        int showUp, PerHour, total, totalAmount, totalHoursAmount;
+//
+//        showUp = 75;
+//        PerHour = 125;
+//
+//        System.out.print("How many hours did the plumber take while working? ");
+//        total = keyboard.nextInt();
+//        totalHoursAmount = total * PerHour;
+//
+//        totalAmount = totalHoursAmount + showUp;
+//
+//        System.out.println("The total hours the plumber spent while working are: " + total);
+//        System.out.println("Hence the total amount the plumber will be paid will be :" + totalAmount);
+//        int years, anniversary;
+//
+//        System.out.print("How many years have you been married? ");
+//        years = keyboard.nextInt();
+//        anniversary = years / 4;
+//
+//        System.out.println("The total number of anniversaries celebrated together are: " + anniversary);
+
+//        System.out.println("Hello World!");
+//        int gumballs = 27;
+//
+//        gumballs++;
+//        System.out.println(gumballs);
+//        System.out.println(++gumballs);
+//        System.out.println(gumballs);
+//        int i = 10;
+//        System.out.println(i++);
+//        System.out.println(--i);
+//        --i;
+//        i--;
+//        System.out.println(i++);
+//        System.out.println(++i);
+//        System.out.println(i--);
+//        System.out.println(i);
+//        kids = kids + 1;
+////        kids +=1;
+//        double amount = 5.95;
+//        double shippingAndHandling = 25.00, discount = 0.15;
+//        amount += shippingAndHandling;
+//        amount = discount * 2;
+//
+//        System.out.println(amount);
+//        int i = 10; // i starts counting from the first assignment all the way to the last
+//        i += 2;
+//        i -= 5;
+//        i *= 6; //this now becomes the final value of i after all the operations are done.
+//        System.out.println(i);
+//        System.out.println(i += 3);
+//        System.out.println(i %= 2);
+
+//        smallLetter = 'b';
+//        bigLetter = Character.toUpperCase(smallLetter);
+////        System.out.println(bigLetter);
+//        char smallLetter, bigLetter;
+//
+//        smallLetter = 'R';
+//        bigLetter = Character.toUpperCase(smallLetter);
+//        System.out.println(bigLetter);
+//
+//        smallLetter = '3';
+//        bigLetter = Character.toUpperCase(smallLetter);
+//        System.out.println(bigLetter);
