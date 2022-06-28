239d77b071dd91167c025be8913473887ee39503
VincentGichohi <vincentegichohi@gmail.com>
2022-06-28 12:08:19 +0300

    Create a winLose class

---
+++ src/WinLoseOrTie.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import static java.lang.System.out;
+
+public class WinLoseOrTie {
+    public static void main(String[] args) {
+        var keyboard = new Scanner(System.in);
+        int hankees, socks;
+
+        out.print("Hankees and Socks scores? ");
+        hankees = keyboard.nextInt();
+        socks = keyboard.nextInt();
+        out.println();
+
+        if (hankees > socks) {
+            out.println("Hankees win ....");
+            out.print("Hankees : ");
+            out.println(hankees);
+            out.print("Socks:  ");
+            out.println(socks);
+        } else if (socks > hankees) {
+            out.println("Socks win ....");
+            out.print("Hankees: ");
+            out.println(hankees);
+            out.print("Socks:  ");
+            out.println(socks);
+        }
+
+        keyboard.close();
+    }
+}
