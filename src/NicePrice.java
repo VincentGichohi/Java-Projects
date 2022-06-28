9181e5bc81d0cd154abffcdd0b89d4560adf1f77
VincentGichohi <vincentegichohi@gmail.com>
2022-06-28 11:32:26 +0300

    Add a NicePrice Class

--- src/NicePrice.java
+++ src/NicePrice.java
@@ -1,2 +1,41 @@
-package PACKAGE_NAME;public class NicePrice {
+import java.util.Scanner;
+
+public class NicePrice {
+    public static void main(String[] args) {
+        var keyboard = new Scanner(System.in);
+        int age;
+        double price = 0.00;
+        char reply;
+        boolean isKid, isSenior, hasCoupon, hasNoCoupon;
+
+        System.out.print("How old are you? ");
+        age = keyboard.nextInt();    //stores the age inputted above
+
+        System.out.print("Have a coupon? (y/N) ");
+        reply = keyboard.findWithinHorizon(".", 0).charAt(0);
+
+        isKid = age < 12;
+        isSenior = age >= 65;
+        hasCoupon = reply == 'Y' || reply == 'y';
+        hasNoCoupon = reply == 'N' || reply == 'n';
+
+        if (!isKid && !isSenior) {
+            price = 9.25;
+        }
+        if (isKid || isSenior) {
+            price = 5.25;
+        }
+        if (hasCoupon) {
+            price -=2.00;
+        }
+        if (!hasCoupon && !hasNoCoupon) {
+            System.out.println("Huh?? ");
+        }
+        System.out.print("Please pay $");
+        System.out.print(price);
+        System.out.print(" . ");
+        System.out.print("Enjoy the show!");
+
+        keyboard.close();
+    }
 }
