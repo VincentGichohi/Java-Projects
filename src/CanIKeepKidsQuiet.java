f01774d3f6e87015573eb1432697188c93566028
VincentGichohi <vincentegichohi@gmail.com>
2022-07-02 23:34:38 +0300

    Create a new class program

--- src/CanIKeepKidsQuiet.java
+++ src/CanIKeepKidsQuiet.java
@@ -1,19 +1,33 @@
+
+import java.util.Scanner;
 public class CanIKeepKidsQuiet {
 
     public static void main(String[] args) {
-        int gumballs;
-        int kids;
-        int gumballsPerKid;
-        boolean eachKidGetsTen;
+//        int gumballs;
+//        int kids;
+//        int gumballsPerKid;
+//        boolean eachKidGetsTen;
+//
+//        gumballs = 140;
+//        kids = 15;
+//        gumballsPerKid = gumballs / kids;
+//
+//        System.out.print("True or False? ");
+//        System.out.println("Each kid gets 10 gumballs. ");
+//        eachKidGetsTen = gumballsPerKid >= 10;
+//        System.out.println(eachKidGetsTen);
 
-        gumballs = 140;
-        kids = 15;
-        gumballsPerKid = gumballs / kids;
+        var keyboard = new Scanner(System.in);
+        char symbol;
 
-        System.out.print("True or False? ");
-        System.out.println("Each kid gets 10 gumballs. ");
-        eachKidGetsTen = gumballsPerKid >= 10;
-        System.out.println(eachKidGetsTen);
+        symbol = keyboard.findWithinHorizon(".",0).charAt(0);
 
+        while (symbol != '@') {
+            System.out.print(symbol);
+            symbol = keyboard.findWithinHorizon(".",0).charAt(0);
+        }
+        System.out.println();
+
+        keyboard.close();
     }
 }
