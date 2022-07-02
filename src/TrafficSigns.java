148c788e8ba6fc7a12507bcbbefb669236ac913d
VincentGichohi <vincentegichohi@gmail.com>
2022-07-02 19:47:56 +0300

    Change the outlook of the code

--- src/TrafficSigns.java
+++ src/TrafficSigns.java
@@ -1,20 +1,20 @@
-import java.util.Scanner;
-import static java.lang.System.out;
-
-public class TrafficSigns {
-    public static void main(String[] args) {
-        var keyboard = new Scanner(System.in);
-//        char Y, N;
-//        char approach, proceed, direction;
-
-
-        out.print("Are you approaching a green light? Type 'Y' for yes and 'N' for no.");
-        aproach = keyboard.nextLine();
-
-        out.print("Is it safe to proceed through the intersection? Type 'Y' for yes and 'N' for no.");
-        proceed = keyboard.nextLine();
-
-
-
-    }
-}
+//import java.util.Scanner;
+//import static java.lang.System.out;
+//
+//public class TrafficSigns {
+//    public static void main(String[] args) {
+//        var keyboard = new Scanner(System.in);
+////        char Y, N;
+////        char approach, proceed, direction;
+//
+//
+//        out.print("Are you approaching a green light? Type 'Y' for yes and 'N' for no.");
+//        aproach = keyboard.nextLine();
+//
+//        out.print("Is it safe to proceed through the intersection? Type 'Y' for yes and 'N' for no.");
+//        proceed = keyboard.nextLine();
+//
+//
+//
+//    }
+//}
