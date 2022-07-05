3e50f0b6e517c8670c32ee6cf6d82684fa55d657
VincentGichohi <vincentegichohi@gmail.com>
2022-07-05 12:41:03 +0300

    Add print statements to the class

--- src/ProcessData.java
+++ src/ProcessData.java
@@ -3,8 +3,12 @@ import java.util.Scanner;
 public class ProcessData {
     public static void main(String[] args) {
         var keyboard =  new Scanner(System.in);
-        double UnitPrice;
+        double unitPrice;
         int quantity;
         boolean taxable;
+
+        System.out.print("Unit Price: ");
+        unitPrice = keyboard.nextDouble();
+        System.out.print("Quantity: ");
     }
 }
