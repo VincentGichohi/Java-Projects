a8c7fdad37fec86a76d8021cf12b438095359b35
VincentGichohi <vincentegichohi@gmail.com>
2022-07-05 12:44:36 +0300

    Add an if statement

--- src/ProcessData.java
+++ src/ProcessData.java
@@ -10,5 +10,17 @@ public class ProcessData {
         System.out.print("Unit Price: ");
         unitPrice = keyboard.nextDouble();
         System.out.print("Quantity: ");
+        quantity = keyboard.nextInt();
+        System.out.print("Taxable? (true/false) ");
+        taxable = keyboard.nextBoolean();
+
+        double total = unitPrice * quantity;
+        if (taxable) {
+            total *= 1.05;
+        }
+        System.out.print("Total: ");
+        System.out.println(total);
+
+        keyboard.close();
     }
 }
