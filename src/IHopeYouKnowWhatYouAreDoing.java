8f3ba8c9292aa8e6a1e6c1af7a558999d43d7c07
VincentGichohi <vincentegichohi@gmail.com>
2022-07-05 09:55:05 +0300

    Add a new class

--- src/IHopeYouKnowWhatYouAreDoing.java
+++ src/IHopeYouKnowWhatYouAreDoing.java
@@ -1,8 +1,29 @@
+import java.io.File;
 import java.util.Scanner;
-
 public class IHopeYouKnowWhatYouAreDoing {
     public static void main(String[] args) {
+        var keyboard = new Scanner(System.in);
+        char reply;
+        int guess;
+
+        do {
+            System.out.print("Reply with Y or N.....");
+            System.out.print(" Delete the important Data File? ");
+            reply = keyboard.findWithinHorizon(". ", 0).charAt(0);
+        } while (reply != 'Y' && reply != 'N');
+
+            if (reply == 'Y') {
+                new File ("importantData.txt").delete();
+                System.out.println("Deleted!");
+            } else {
+                System.out.println("No harm in asking!");
+//            }
+//        do {
+//            System.out.print("Guess a number: ");
+//            guess = keyboard.nextInt();
+//        } while (guess )
+        keyboard.close();
 
     }
-
 }
+
