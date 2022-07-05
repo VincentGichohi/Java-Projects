f1c0662154f395105a4b069aa43b1d7fa9124ea5
VincentGichohi <vincentegichohi@gmail.com>
2022-07-05 12:38:54 +0300

    Add values to the class

--- src/ProcessData.java
+++ src/ProcessData.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class ProcessData {
+    public static void main(String[] args) {
+        var keyboard =  new Scanner(System.in);
+        double UnitPrice;
+        int quantity;
+        boolean taxable;
+    }
 }
