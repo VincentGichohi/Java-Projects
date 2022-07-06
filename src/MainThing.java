a142bf28e98f71e5953c388b84431835e6def265
VincentGichohi <vincentegichohi@gmail.com>
2022-07-06 12:50:22 +0300

    Create a string format

--- src/MainThing.java
+++ src/MainThing.java
@@ -5,6 +5,15 @@ public class MainThing {
         var keyboard =  new Scanner(System.in);
         Thing oneThing =  new Thing();
 
-    }
+        System.out.print("Enter the first number : ");
+        oneThing.value1 = keyboard.nextInt();
 
+        System.out.print("Enter the second number: ");
+        oneThing.value2 = keyboard.nextInt();
+
+        String str = String.format("The value of numbers entered are: %.2f, %.2f", oneThing.value1, oneThing.value2);
+        System.out.println(str);
+
+        keyboard.close();
+    }
 }
