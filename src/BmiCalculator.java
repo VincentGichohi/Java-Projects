111aab69e39804a050bf2960e6784db48cbdd06a
VincentGichohi <vincentegichohi@gmail.com>
2022-07-06 10:46:22 +0300

    Print the bmi rounded off

--- src/BmiCalculator.java
+++ src/BmiCalculator.java
@@ -3,7 +3,18 @@ import java.util.Scanner;
 public class BmiCalculator {
     public static void main(String[] args) {
         var keyboard = new Scanner(System.in);
-        double weight, height;
+        Person onePerson = new Person();
 
+        System.out.print("Welcome to the BMI Calculator. Input your weight and height for calculations.");
+        System.out.print("Enter your weight: ");
+        onePerson.weight = keyboard.nextDouble();
+        System.out.print("Enter your height: ");
+        onePerson.height = keyboard.nextDouble();
+
+        double total = onePerson.weight / onePerson.height;
+        System.out.print("Your BMI is: ");
+        System.out.println(Math.round(total));
+
+        keyboard.close();
     }
 }
