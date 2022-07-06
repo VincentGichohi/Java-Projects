41c68cd2dfa8e676a5420dea883e85caf0d5844e
VincentGichohi <vincentegichohi@gmail.com>
2022-07-06 12:13:20 +0300

    Link the class thing to main thing

--- src/MainThing.java
+++ src/MainThing.java
@@ -1,2 +1,10 @@
+import java.util.Scanner;
+
 public class MainThing {
+    public static void main(String[] args) {
+        var keyboard =  new Scanner(System.in);
+        Thing oneThing =  new Thing();
+
+    }
+
 }
