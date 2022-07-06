92db55ff6615ce3f61b7ba4112d7619493af00fc
VincentGichohi <vincentegichohi@gmail.com>
2022-07-06 21:49:39 +0300

    Change the code for the change password

--- src/CheckPassword.java
+++ src/CheckPassword.java
@@ -1,7 +1,20 @@
 import java.util.Scanner;
-
 public class CheckPassword {
     public static void main(String[] args) {
         var keyboard = new Scanner(System.in);
+//        String Password = "swordfish";
+//        String userInput;
+//
+//        System.out.print("What's the password? ");
+//        userInput = keyboard.next();
+//
+//        if (Password.equals(userInput)) {
+//            System.out.println("You're okay!");
+//        } else {
+//            System.out.println("You are a menace.");
+//        }
+
+
+        keyboard.close();
     }
 }
