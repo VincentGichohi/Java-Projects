89c601a73c4b6f11a82e2b0a1e5af490a84274bf
VincentGichohi <vincentegichohi@gmail.com>
2022-07-06 21:52:13 +0300

    Create a new class SetFrame

--- src/SetFrame.java
+++ src/SetFrame.java
@@ -1,2 +1,13 @@
+import javax.swing.JFrame;
+
 public class SetFrame {
+    public static void main(String[] args) {
+        JFrame frame =  new JFrame();
+        frame.setSize(300,300);
+        frame.setTitle("This is a frame");
+        frame.setVisible(true);
+
+
+    }
+
 }
