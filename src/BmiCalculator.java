e35c24adeaa66d06dac36b327738871b67831909
VincentGichohi <vincentegichohi@gmail.com>
2022-07-06 09:19:55 +0300

    Add a bmicalculator class

--- src/BmiCalculator.java
+++ src/BmiCalculator.java
@@ -1,2 +1,9 @@
+import java.util.Scanner;
+
 public class BmiCalculator {
+    public static void main(String[] args) {
+        var keyboard = new Scanner(System.in);
+        double weight, height;
+
+    }
 }
