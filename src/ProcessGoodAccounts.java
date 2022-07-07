05f2ece64b235b4cc622ed7295685a8b19f459aa
VincentGichohi <vincentegichohi@gmail.com>
2022-07-08 00:08:16 +0300

    Add variables

--- src/ProcessGoodAccounts.java
+++ src/ProcessGoodAccounts.java
@@ -5,6 +5,11 @@ import java.text.NumberFormat;
 public class ProcessGoodAccounts {
 
     public static void main(String[] args) {
+        var myRandom =  new Random();
+        var currency = NumberFormat.getCurrencyInstance();
+        GoodAccount anAccount;
+        double interestRate;
+        double yearlyInterest;
 
     }
 }
