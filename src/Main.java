5844ff6140fdaac7edef37227cbeea6309a27eac
VincentGichohi <vincentegichohi@gmail.com>
2022-07-07 09:52:15 +0300

    Add new variables

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import javax.swing.*;
 import java.text.NumberFormat;
 public class Main {
     public static void main(String[] args) {
-
+        var currency = NumberFormat.getCurrencyInstance();
+        String inputString = JOptionPane.showInputDialog("Enter an amount");
+        double inputAmount = Double.parseDouble(inputString);
+        double oneMore = inputAmount + 1;
+        String oneMoreMoney = currency.format(oneMore);
+        String message = "One more than that amount is " + oneMoreMoney + ".";
+        JOptionPane.showMessageDialog(null, message);
     }
 }
