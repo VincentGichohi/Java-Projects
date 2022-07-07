ffc1e4d24e3b019649cebb27b10eb90bbb9dce6f
VincentGichohi <vincentegichohi@gmail.com>
2022-07-07 22:52:05 +0300

    Make some changes to the code

--- src/OneMain.java
+++ src/OneMain.java
@@ -1,2 +1,18 @@
+//import java.util.Scanner;
+
 public class OneMain {
+    public static void main(String[] args) {
+        var holder1 = new IntegerHolder();
+        holder1.value = 79;
+        IntegerHolder.howMany++;
+
+        var holder2 = new IntegerHolder();
+        holder2.value = 443;
+        IntegerHolder.howMany++;
+
+        System.out.println(holder1.value);
+        System.out.println(holder2.value);
+        System.out.println(IntegerHolder.howMany);
+
+    }
 }
