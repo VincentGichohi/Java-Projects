9ec1f351ecad66947b19d6c50a652818f900e43c
VincentGichohi <vincentegichohi@gmail.com>
2022-07-07 09:24:45 +0300

    Add a class using the NumberFormat

--- src/BetterProcessData.java
+++ src/BetterProcessData.java
@@ -6,6 +6,26 @@ public class BetterProcessData {
        var keyboard = new Scanner(System.in);
        double unitPrice;
        int quantity;
+       boolean taxable;
 
+       NumberFormat currency = NumberFormat.getCurrencyInstance();
+
+        System.out.print("Unit Price: ");
+        unitPrice = keyboard.nextDouble();
+        System.out.print("Quantity: ");
+        quantity = keyboard.nextInt();
+        System.out.print("taxable? (true/false) ");
+        taxable = keyboard.nextBoolean();
+
+        double total = unitPrice * quantity;
+        if (taxable) {
+            total = total * 1.05;
+        }
+
+        String niceTotal = currency.format(total);
+        System.out.print("Total:  ");
+        System.out.println(niceTotal);
+
+        keyboard.close();
     }
 }
