fe1284d7ffc59f06ff67be87d95ed7cf943bba0c
VincentGichohi <vincentegichohi@gmail.com>
2022-07-08 00:03:52 +0300

    Create two methods

--- src/GoodAccount.java
+++ src/GoodAccount.java
@@ -1,2 +1,31 @@
+
+import java.text.NumberFormat;
+import static java.lang.System.out;
+
 public class GoodAccount {
+    String lastName;
+    int id;
+    double balance;
+
+    double getInterest(double rate) {
+        double interest;
+
+        out.print("Adding ");
+        out.print(rate);
+        out.println(" percent ...");
+
+        interest = balance * (rate / 100);
+        return interest;
+    }
+
+    void display() {
+        var currency = NumberFormat.getCurrencyInstance();
+
+        out.print("The account with the last name ");
+        out.print(lastName);
+        out.print(" and ID number ");
+        out.print(" has balance ");
+        out.println(currency.format(balance));
+    }
 }
+
