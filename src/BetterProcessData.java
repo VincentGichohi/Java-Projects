5a57d750f54ca5b231a9a0d504177d4351199eee
VincentGichohi <vincentegichohi@gmail.com>
2022-07-07 08:37:06 +0300

    Add a scanner variable

--- src/BetterProcessData.java
+++ src/BetterProcessData.java
@@ -1,2 +1,11 @@
+import java.util.Scanner;
+import java.text.NumberFormat;
+
 public class BetterProcessData {
+    public static void main(String[] args) {
+       var keyboard = new Scanner(System.in);
+       double unitPrice;
+       int quantity;
+
+    }
 }
