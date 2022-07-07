757a331033a74ebfcc5eca2baa49396f62887c7b
VincentGichohi <vincentegichohi@gmail.com>
2022-07-07 23:01:30 +0300

    Add two variables

--- src/ProcessNiceAccounts.java
+++ src/ProcessNiceAccounts.java
@@ -2,6 +2,9 @@
 import java.util.Random;
 public class ProcessNiceAccounts {
     public static void main(String[] args) {
+        var myRandom = new Random();
+        NiceAccount  anAccount;
+        double InterestRate;
 
     }
 }
