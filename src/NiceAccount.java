cfcf30239058819533579e12c6acde921962db81
VincentGichohi <vincentegichohi@gmail.com>
2022-07-07 22:57:42 +0300

    Add a new class

---
+++ src/NiceAccount.java
@@ -0,0 +1,28 @@
+
+import java.text.NumberFormat;
+import static java.lang.System.out;
+
+public class NiceAccount {
+    String lastName;
+    int id;
+    double balance;
+
+    void addInterest(double rate) {
+        out.print("Adding ");
+        out.print(rate);
+        out.println(" percent ....");
+
+        balance += balance * (rate / 100);
+    }
+
+    void display() {
+        var currency = NumberFormat.getCurrencyInstance();
+
+        out.print(" The account with the last name ");
+        out.print(lastName);
+        out.print(" and ID number ");
+        out.print(id);
+        out.print(" has balance ");
+        out.println(currency.format(balance));
+    }
+}
