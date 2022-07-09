896849aff004279df03cc85da2796a81a8cadd18
VincentGichohi <vincentegichohi@gmail.com>
2022-07-09 17:15:17 +0300

    Create a new class with contents in them

--- src/ReadAndWrite.java
+++ src/ReadAndWrite.java
@@ -1,2 +1,24 @@
+
+import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 public class ReadAndWrite {
+    public static void main(String[] args) throws FileNotFoundException{
+        var diskScanner = new Scanner(new File("rawData.txt"));
+        var diskWriter = new PrintStream("cookedData.txt");
+        double unitPrice, total;
+        int quantity = 0;
+
+        unitPrice = diskScanner.nextDouble();
+        quantity =  diskScanner.nextInt();
+
+        total = unitPrice * quantity;
+
+        diskWriter.println(total);
+
+        diskScanner.close();
+        diskWriter.close();
+    }
 }
