0a3e645b8231ae157bc1c23627dc9b367fe8d1a4
VincentGichohi <vincentegichohi@gmail.com>
2022-07-11 22:18:24 +0300

    Add cases to the switch statement

--- src/TheNewSwitcheroo.java
+++ src/TheNewSwitcheroo.java
@@ -13,6 +13,24 @@ public class TheNewSwitcheroo {
         keyboard.nextLine();
         randomNumber = myRandom.nextInt(10) + 1;
 
+        output = switch(randomNumber) {
+            case 1 -> "Yes. Isn't it obvious? ";
+            case 2 -> "What part of 'mo don't you understand? ";
+            case 3 -> "Yessir, yessir! Three bags full.";
+            case 4,5 -> "No, and don't ask again.";
+            case 6 -> "Sure whatever.";
+            case 7 -> "Yes, but only if you're nice to me.";
+            case 8 -> """
+                    Yes (as if you care).
+                    Next time, take your problem and somewhere else.
+                    """;
+            case 9 -> "No until Cromwell seizes Dover.";
+            case 10 -> "No, until Nell squeezes Rover.";
+            default -> "My random number generator is broken!";
+        };
+        out.println(output);
+
+        keyboard.close();
     }
 
 }
