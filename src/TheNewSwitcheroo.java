9ed69cf0c56f9cd133a094b2675e8e0c2e9a242b
VincentGichohi <vincentegichohi@gmail.com>
2022-07-11 21:49:31 +0300

    Create a new random and scanner class

--- src/TheNewSwitcheroo.java
+++ src/TheNewSwitcheroo.java
@@ -4,6 +4,9 @@ import java.util.Scanner;
 public class TheNewSwitcheroo {
     public static void main(String[] args) {
         var keyboard = new Scanner(System.in);
+        var myRandom =  new Random();
+        int randomNumber;
+        String output;
     }
 
 }
