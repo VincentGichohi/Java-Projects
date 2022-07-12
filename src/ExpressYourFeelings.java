17971b785b3a4b4eb1c377519baaaf2134b83315
VincentGichohi <vincentegichohi@gmail.com>
2022-07-12 09:50:31 +0300

    Change the expressyourfeelings code

--- src/ExpressYourFeelings.java
+++ src/ExpressYourFeelings.java
@@ -1,7 +1,35 @@
 import java.util.Scanner;
 
+import static java.lang.System.out;
+
 public class ExpressYourFeelings {
     public static void main(String[] args) {
-        var keyboard =  new Scanner (System.in);
+        var keyboard =  new Scanner(System.in);
+        String month;
+        int numberOfDays = 0;
+        boolean isLeapYear;
+
+        out.print("Which month? ");
+        month = keyboard.next();
+
+        switch(switch(month) {
+            case "January", "March", "May", "July", "August", "October", "December" -> numberOfDays = 31;
+            case "April", "June", "September", "November" -> numberOfDays = 30;
+            case "February" -> {
+                out.print("Leap year (true/false) ? ");
+                isLeapYear = keyboard.nextBoolean();
+                if (isLeapYear) {
+                     yield 29;
+                } else {
+                    yield 28;
+                }
+            }
+            default -> 0;
+        } + "days");
+//        out.print("The number of days are: ");
+//        out.print(numberOfDays);
+//        out.println(" days ");
+
+        keyboard.close();
     }
 }
