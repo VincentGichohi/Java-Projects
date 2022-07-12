c0892a06577c58bd7467ef01e67454fbbeefd3e2
VincentGichohi <vincentegichohi@gmail.com>
2022-07-12 08:43:07 +0300

    Add keyboard and random variables

--- src/NewDaysOfTheWeek.java
+++ src/NewDaysOfTheWeek.java
@@ -4,6 +4,7 @@ import java.util.Scanner;
 import static java.lang.System.out;
 public class NewDaysOfTheWeek {
     public static void main(String[] args) {
-
+        var keyboard =  new Scanner(System.in);
+        var myRandom =  new Random();
     }
 }
