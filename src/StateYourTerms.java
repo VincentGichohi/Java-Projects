4423ce6baa98d35af2956ad00d707169c374ed3d
VincentGichohi <vincentegichohi@gmail.com>
2022-07-12 09:14:17 +0300

    Add a keyboard variable and boolean values

--- src/StateYourTerms.java
+++ src/StateYourTerms.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
+import static java.lang.System.out;
 
 public class StateYourTerms {
     public static void main(String[] args) {
-
+        var keyboard =  new Scanner(System.in);
+        String month;
+        int numberOfDays = 0;
+        boolean isLeapYear;
     }
 
 }
